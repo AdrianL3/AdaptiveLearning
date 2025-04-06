2c831b664e8ff85091238bc2cb3f7bc8a14a3413
Daniel Poedyasmara <dpoedyasmara@Daniels-MacBook-Pro-5.local>
2025-04-05 17:46:51 -0700

    scrap

--- client/src/components/DashHeader.js
+++ client/src/components/DashHeader.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import logo from '../NavigationLogo.png'; // Ensure the path is correct
 
 const DashHeader = () => {
     const content = (
         <header className="dash-header">
             <div className="dash-header__container">
                 <Link to="/dash">
-                    <h1 className="dash-header__title">techNotes</h1>
+                    <a href="/dash"><img src={logo}></img></a>
+                    <h1 className="dash-header__title">notes</h1>
                 </Link>
                 <nav className="dash-header__nav">
                     {/* add nav buttons later */}
